a559713e847e3686ac394a669713f0a8cede4ef3
Protechy651 <166385058+Protechy651@users.noreply.github.com>
2024-04-09 13:42:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config(object):
       API_HASH = getenv("API_HASH", "6057e930c3b495c98e979c1df311d6c3")
       API_ID = int(getenv("API_ID", "28037792"))
       AS_COPY = True if getenv("AS_COPY", True) == "`{file_name}`" else True
-      BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001722984461:-1001623633000").replace("\n", " ").split(' '))
+      BOT_TOKEN = getenv("BOT_TOKEN", "6963108191:AAHR3WG0jcKxFzGWht6c9MF3nutwy4boeYs")
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1002022758511:-1002100847281").replace("\n", " ").split(' '))
 
 
 # Don't Remove Credit @VJ_Botz
